0cbce8b458122d804f3c6c6d991defbbc3c8a15d
gustavvopenna <gustavo.pema@gmail.com>
2020-08-23 13:18:47 -0500

    Add favorite and copy to clipboard icons to each gif

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="flex">
-    <div v-for="giph in giphs" :key="giph.id" class="rounded bg-gray-100 mx-1">
-      <h1 class="font-bold text-2xl">{{ giph.title }}</h1>
-      <video :src="giph.images.downsized_small.mp4" autoplay loop />
+  <div class="flex flex-wrap">
+    <div
+      v-for="giph in giphs"
+      :key="giph.id"
+      class="group relative rounded bg-gray-100 m-1 overflow-hidden"
+    >
+      <img :src="giph.images.original.url" class="h-40" />
+      <div class="flex absolute justify-end bottom-0 w-full p-2">
+        <ClipboardCopyIcon
+          class="mr-2 inline-block opacity-0 group-hover:opacity-100 hover:text-blue-700 text-white transition ease-in-out duration-200 cursor-pointer"
+        />
+        <HeartIcon
+          class="inline-block opacity-0 group-hover:opacity-100 hover:text-red-600 text-white transition ease-in-out duration-200 cursor-pointer"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// Icons
+import ClipboardCopyIcon from "../assets/icons/clipboard-copy.svg";
+import HeartIcon from "../assets/icons/heart.svg";
+
 export default {
   name: "HelloWorld",
+  components: {
+    ClipboardCopyIcon,
+    HeartIcon
+  },
   data() {
     return {
       giphs: null
@@ -20,10 +39,11 @@ export default {
   },
   methods: {
     async getGiphs() {
-      console.log("1");
-      console.log(this.$gf);
-      const { data } = await this.$gf.trending({ limit: 10 });
-      console.log(data, "data");
+      const { data } = await this.$gf.search("dogs", {
+        sort: "relevant",
+        limit: 10
+      });
+      console.log(data);
       this.giphs = data;
     }
   }
