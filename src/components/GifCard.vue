3be4d456568cd18f81a507073898618022b768f9
gustavvopenna <gustavo.pema@gmail.com>
2020-08-25 10:39:58 -0500

    Create gift card component to handle load state on each gif

---
+++ src/components/GifCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="w-1/2 sm:w-auto">
+    <transition name="fade">
+      <div v-show="isLoad" class="group relative rounded bg-gray-100 m-1 overflow-hidden">
+        <img @load="loaded" :src="image" class="h-auto sm:h-48 bg-gray-100" />
+        <div class="group flex absolute justify-end top-0 sm:top-auto sm:bottom-0 w-full p-2">
+          <ClipboardCopyIcon
+            class="transform mr-2 inline-block opacity-0 group-hover:opacity-100 hover:scale-110 text-white transition ease-in-out duration-200 cursor-pointer"
+          />
+          <HeartIcon
+            class="transform inline-block opacity-0 group-hover:opacity-100 hover:scale-110 text-white transition ease-in-out duration-200 cursor-pointer"
+          />
+        </div>
+      </div>
+    </transition>
+    <div
+      v-show="!isLoad"
+      class="m-1 sm:w-40 h-20 sm:h-48 bg-gray-100 flex justify-center items-center"
+    >
+      <Loader />
+    </div>
+  </div>
+</template>
+
+<script>
+// Components
+import Loader from "../components/Loader";
+
+// Icons
+import ClipboardCopyIcon from "../assets/icons/clipboard-copy.svg";
+import HeartIcon from "../assets/icons/heart.svg";
+
+export default {
+  name: "GifCard",
+  components: {
+    Loader,
+    ClipboardCopyIcon,
+    HeartIcon
+  },
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isLoad: false
+    };
+  },
+  methods: {
+    loaded() {
+      this.isLoad = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
