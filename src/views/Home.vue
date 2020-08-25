ace0853610747be67f0a1825b229772b3c3b3d12
gustavvopenna <gustavo.pema@gmail.com>
2020-08-25 10:40:13 -0500

    Create Loader component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="home max-w-6xl mx-auto">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home sm:max-w-6xl mx-auto max-w-full px-2 sm:px-0">
+    <SearchBar />
+    <h1 class="text-4xl text-gray-900 font-bold capitalize">{{ $store.state.currentSearch }}</h1>
+    <Gifs msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Gifs from "@/components/Gifs.vue";
+import SearchBar from "@/components/SearchBar.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Gifs,
+    SearchBar
   }
 };
 </script>
