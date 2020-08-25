1ae9b5569031beee812714796ec226bb7112408b
gustavvopenna <gustavo.pema@gmail.com>
2020-08-25 10:35:20 -0500

    Create search bar component and vuex store to manage current search

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit="handleSubmit" class="mb-2">
+    <label class="inline-flex items-center relative w-full sm:w-auto">
+      <input
+        name="search"
+        type="text"
+        v-model="search"
+        placeholder="Search gifs"
+        class="w-full px-4 sm:w-auto bg-gray-200 text-gray-600 rounded p-2 outline-none focus:shadow-outline"
+      />
+      <button type="submit" class="absolute right-0 mr-1 outline-none focus:outline-none">
+        <SearchIcon class="text-gray-600" />
+      </button>
+    </label>
+  </form>
+</template>
+
+<script>
+// TODO: check if Label tag as a container is "good practice"
+
+// Libraries
+import { mapMutations } from "vuex";
+
+// Icons
+import SearchIcon from "../assets/icons/search.svg";
+
+export default {
+  name: "SearchBar",
+  components: {
+    SearchIcon
+  },
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    ...mapMutations(["setCurrentSearch"]),
+    handleSubmit(e) {
+      e.preventDefault();
+      this.setCurrentSearch(this.search);
+      this.search = "";
+    }
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    currentSearch: 'dogs'
+  },
+  mutations: {
+    setCurrentSearch(state, search) {
+      state.currentSearch = search
+    }
+  },
   actions: {},
   modules: {}
 });
