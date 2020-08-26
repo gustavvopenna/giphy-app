2ce39022f4e170d5dcc31efd230db7a5683097f5
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 18:34:01 -0500

    Fix register and login popup not showing outside Home route, move them to global layout

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,46 @@
       <NavbarDesktop />
       <router-view />
     </div>
+    <Popup @close="handlePopupClose">
+      <Register v-if="$store.state.registerRequested" />
+      <Login v-if="$store.state.loginRequested" />
+    </Popup>
   </div>
 </template>
 
 <script>
+//Libraries
+import { mapMutations } from "vuex";
+
+// Componets
 import NavbarMobile from "@/components/NavbarMobile.vue";
 import NavbarDesktop from "@/components/NavbarDesktop.vue";
+import Popup from "@/components/Popup.vue";
+import Register from "@/components/Register.vue";
+import Login from "@/components/Login.vue";
+
+// Css
 import "vue-toast-notification/dist/theme-default.css";
 
 export default {
   components: {
     NavbarMobile,
-    NavbarDesktop
+    NavbarDesktop,
+    Popup,
+    Register,
+    Login
+  },
+  methods: {
+    ...mapMutations([
+      "setdisplayPopup",
+      "setRegisterRequested",
+      "setLoginRequested"
+    ]),
+    handlePopupClose() {
+      this.setdisplayPopup(false);
+      this.setRegisterRequested(false);
+      this.setLoginRequested(false);
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,25 @@
 <template>
-  <div class="home pt-20 sm:max-w-5xl mx-auto max-w-full px-2 sm:px-0 sm:ml-64">
-    <h1
-      class="mt-1 mb-2 text-4xl text-gray-900 font-bold capitalize"
-    >{{ $store.state.currentSearch }}</h1>
-    <SearchBar />
-    <Gifs msg="Welcome to Your Vue.js App" />
-    <Popup @close="handlePopupClose">
-      <Register v-if="$store.state.registerRequested" />
-      <Login v-if="$store.state.loginRequested" />
-    </Popup>
+  <div class="sm:ml-64">
+    <div class="home pt-20 sm:max-w-5xl mx-auto max-w-full px-2 sm:px-0">
+      <h1
+        class="mt-1 mb-2 text-4xl text-gray-900 font-bold capitalize"
+      >{{ $store.state.currentSearch }}</h1>
+      <SearchBar />
+      <Gifs />
+    </div>
   </div>
 </template>
 
 <script>
-//Libraries
-import { mapMutations } from "vuex";
-
 // Components
 import SearchBar from "@/components/SearchBar.vue";
 import Gifs from "@/components/Gifs.vue";
-import Popup from "@/components/Popup.vue";
-import Register from "@/components/Register.vue";
-import Login from "@/components/Login.vue";
 
 export default {
   name: "Home",
   components: {
     Gifs,
-    SearchBar,
-    Popup,
-    Register,
-    Login
-  },
-  data() {
-    return {
-      customComponent: "Register"
-    };
-  },
-  mounted() {
-    this.setCustomComponent();
-  },
-  methods: {
-    ...mapMutations([
-      "setdisplayPopup",
-      "setRegisterRequested",
-      "setLoginRequested"
-    ]),
-    handlePopupClose() {
-      this.setdisplayPopup(false);
-      this.setRegisterRequested(false);
-      this.setLoginRequested(false);
-    },
-    setCustomComponent() {
-      if (this.$store.state.loginRequested) this.customComponent = "Login";
-      this.customComponent = "Register";
-    }
+    SearchBar
   }
 };
 </script>
