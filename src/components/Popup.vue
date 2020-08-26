b006633dd1bbc3acad760e0b7753f72da3446871
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 01:52:17 -0500

    Add popup component to Home view to render signup/signin components

---
+++ src/components/Popup.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="$store.state.displayPopup" class="fixed inset-0 bg-gray-800 bg-opacity-98">
+    <div class="w-full h-full flex justify-center items-center">
+      <div class="bg-gray-100 rounded max-w-sm p-4">
+        <div class="text-right">
+          <button
+            @click="close"
+            class="p-2 -mr-2 -mt-2 rounded-full transform ease-in-out duration-200 hover:rotate-90"
+          >
+            <CloseIcon />
+          </button>
+        </div>
+        <div>Hola, bienvenido a Worgiphy</div>
+        <!-- <component v-if="ChildComponent" :is="importedComponent" /> -->
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Icons
+import CloseIcon from "../assets/icons/close.svg";
+
+export default {
+  name: "Popup",
+  components: {
+    CloseIcon
+  },
+  props: {
+    ChildComponent: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      importedComponent: null
+    };
+  },
+  created() {
+    // this.importChildComponent();
+  },
+  methods: {
+    // importChildComponent() {
+    //   this.importedComponent = require(`@/components/${this.ChildComponent}.vue`).default;
+    // },
+    close() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,60 @@
 <template>
-  <div class="home sm:max-w-6xl mx-auto max-w-full px-2 sm:px-0">
+  <div class="home pt-20 sm:max-w-5xl mx-auto max-w-full px-2 sm:px-0 sm:ml-64">
+    <h1
+      class="mt-1 mb-2 text-4xl text-gray-900 font-bold capitalize"
+    >{{ $store.state.currentSearch }}</h1>
     <SearchBar />
-    <h1 class="text-4xl text-gray-900 font-bold capitalize">{{ $store.state.currentSearch }}</h1>
     <Gifs msg="Welcome to Your Vue.js App" />
+    <Popup @close="handlePopupClose">
+      <Register v-if="$store.state.registerRequested" />
+      <Login v-if="$store.state.loginRequested" />
+    </Popup>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Gifs from "@/components/Gifs.vue";
+//Libraries
+import { mapMutations } from "vuex";
+
+// Components
 import SearchBar from "@/components/SearchBar.vue";
+import Gifs from "@/components/Gifs.vue";
+import Popup from "@/components/Popup.vue";
+import Register from "@/components/Register.vue";
+import Login from "@/components/Login.vue";
 
 export default {
   name: "Home",
   components: {
     Gifs,
-    SearchBar
+    SearchBar,
+    Popup,
+    Register,
+    Login
+  },
+  data() {
+    return {
+      customComponent: "Register"
+    };
+  },
+  mounted() {
+    this.setCustomComponent();
+  },
+  methods: {
+    ...mapMutations([
+      "setdisplayPopup",
+      "setRegisterRequested",
+      "setLoginRequested"
+    ]),
+    handlePopupClose() {
+      this.setdisplayPopup(false);
+      this.setRegisterRequested(false);
+      this.setLoginRequested(false);
+    },
+    setCustomComponent() {
+      if (this.$store.state.loginRequested) this.customComponent = "Login";
+      this.customComponent = "Register";
+    }
   }
 };
 </script>
