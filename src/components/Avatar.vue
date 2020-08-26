8376612b866954b47e04549be6f0135981b3a749
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 16:57:18 -0500

    Create reusable Avatar component to remove duplicated styles on the Navbar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="{ ...customClasses }">
+    <img :src="image" class="object-cover h-full" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: {
+    border: {
+      type: Boolean,
+      default: false
+    },
+    image: {
+      type: String,
+      default: require("../assets/images/giphy.webp")
+    },
+    size: {
+      type: String,
+      default: null,
+      validator(value) {
+        return ["sm", "md", "lg"].includes(value);
+      }
+    }
+  },
+  computed: {
+    customClasses() {
+      return {
+        // Default styles
+        "rounded-full bg-gray-100 overflow-hidden": true,
+        // Default styles if size prop is not defined
+        "h-10 w-10": !this.size,
+        // Styles apply for 'sm' size
+        "w-10 h-10 ": this.size === "sm",
+        // Styles apply for 'md' size
+        "h-16 w-16": this.size === "md",
+        // Styles apply for 'lg' size
+        "h-20 w-20": this.size === "lg",
+        // styles if border prop is true
+        "border-2 border-gray-100": this.border
+      };
+    }
+  }
+};
+</script>
