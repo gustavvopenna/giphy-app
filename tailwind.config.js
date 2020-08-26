618b575d8ace47cb5c06214fb9c0a2d5ec7b77e8
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 01:46:51 -0500

    Add new colors and opacity to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-1000': '#1D3BDD',
+        'blue-1100': '#08129C',
+        'purple-1000': '#4653cf'
+      },
+      opacity: {
+        '98': '.98'
+      }
+    },
   },
   variants: {
     opacity: ['responsive', 'hover', 'focus', 'group-hover']
