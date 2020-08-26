fcd2c5ff213e4f63d54e5a7e2e74f9750adf2d93
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 18:40:04 -0500

    Fix popup was taking full screen width on mobile

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="$store.state.displayPopup" class="fixed inset-0 bg-gray-800 bg-opacity-98">
     <div class="w-full h-full flex justify-center items-center">
-      <div class="bg-gray-100 rounded max-w-sm p-4">
+      <div class="bg-gray-100 rounded max-w-sm p-4 mx-4 sm:mx-0">
         <div class="text-right">
           <button
             @click="close"
