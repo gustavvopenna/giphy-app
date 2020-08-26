74afecd0d7fdcad6641f04292b1eb1c948f5f468
gustavvopenna <gustavo.pema@gmail.com>
2020-08-26 18:37:39 -0500

    Fix main conteiner not center on desktop

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="h-screen sm:ml-64 flex justify-center items-center">
+    <h1 class="text-2xl">
+      Made with
+      <span class="text-red-600">♥️</span> and
+      <span class="text-green-400 font-extrabold">Ⅴ</span> by
+      <a
+        href="https://www.linkedin.com/in/gustavo-pe%C3%B1a/"
+        target="_blanck"
+        class="text-purple-1000 font-bold"
+      >Gustavo Peña</a>. Click in my name, c'mon...
+    </h1>
   </div>
 </template>
--- src/views/Trending.vue
+++ src/views/Trending.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="home pt-20 sm:max-w-5xl mx-auto max-w-full px-2 sm:px-0 sm:ml-64">
-    <h1 class="mt-1 mb-2 text-4xl text-gray-900 font-bold capitalize">Trending</h1>
-    <div class="flex flex-wrap justify-center">
-      <GifCard v-for="gif in gifs" :key="gif.id" :image="gif.images.original.url" :gif="gif" />
+  <div class="sm:ml-64">
+    <div class="home pt-20 sm:max-w-5xl mx-auto max-w-full px-2 sm:px-0">
+      <h1 class="mt-1 mb-2 text-4xl text-gray-900 font-bold capitalize">Trending</h1>
+      <div class="flex flex-wrap justify-center">
+        <GifCard v-for="gif in gifs" :key="gif.id" :image="gif.images.original.url" :gif="gif" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // Components
-import GifCard from "../components/GifCard";
+import GifCard from "@/components/GifCard";
 
 export default {
   name: "Trending",
